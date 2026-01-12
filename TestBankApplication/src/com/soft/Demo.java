70e43cb360222a682c85c8b99b010d512371a3c8
divyachaudh4 <divyachaudhari553@gmail.com>
2026-01-12 19:20:48 +0530

    Update Demo.java

--- TestBankApplication/src/com/soft/Demo.java
+++ TestBankApplication/src/com/soft/Demo.java
@@ -7,5 +7,7 @@ public class Demo {
 public static void main(String[] args) {
 	System.out.println("welcome to git hub");
 	System.out.println("welcome to sbi application");
+	System.out.println("new changes");
+	
 }
 }
