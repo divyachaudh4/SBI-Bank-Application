b7b1b7df64d3e88d1cb543b3fdc6d481f490a873
divyachaudh4 <divyachaudhari553@gmail.com>
2026-01-12 19:18:04 +0530

    changes in demo

--- TestBankApplication/src/com/soft/Demo.java
+++ TestBankApplication/src/com/soft/Demo.java
@@ -5,6 +5,7 @@ package com.soft;
 public class Demo {
 	
 public static void main(String[] args) {
-	
+	System.out.println("welcome to git hub");
+	System.out.println("welcome to sbi application");
 }
 }
